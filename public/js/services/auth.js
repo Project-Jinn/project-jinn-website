3f1bce33da883859fed842c1b0bff55a6256d622
Jacob <jakegod14@gmail.com>
2017-05-19 12:08:19 -0600

    Quick fix of errors in auth

--- public/js/services/auth.js
+++ public/js/services/auth.js
@@ -2,7 +2,7 @@ var app = angular.module("authModule", ["settings"]);
 
 app.service("authSerivce", function($http, web) {
   this.signup = function(data) {
-    return $http.post(web.host +  "signup", data);
+    return $http.post(web.host +  "auth/signup", data);
   }
   this.signin = function(data) {
     return $http.post(web.host +  "auth/signin", data);
